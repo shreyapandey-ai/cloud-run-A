ae9221202b6f9a9ea68f1f997e10d8437ed88974
Shreya_Pandey_Highspring <shreya.pandey@highspring.in>
2026-02-12 12:13:22 +0530

    Fix formatting issue in main.py

--- main.py
+++ main.py
@@ -249,4 +249,4 @@ def get_history():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",8080))
     app.run(host="0.0.0.0", port=port)
->>>>>>> f50d3e2 (Initial commit: Cloud Run Monitoring App)
+       f50d3e2 (Initial commit: Cloud Run Monitoring App)
